4827984005ed5e09eacf79176f0ddb38befb590c
RongjingH <rrjj.huang@gmail.com>
2024-02-16 00:02:32 -0500

    Start assignment3

---
+++ Assignment3/src/Task.java
@@ -0,0 +1,36 @@
+import java.util.UUID;
+
+public class Task {
+    private final String taskId = UUID.randomUUID().toString();
+    private String taskTitle;
+    private String taskText;
+    private String assignedTo;
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskText() {
+        return taskText;
+    }
+
+    public void setTaskText(String taskText) {
+        this.taskText = taskText;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+}
